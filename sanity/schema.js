d0296dc8937294136b0aca2c189c9f160f9793b5
star-works <farukhkhan1115@gmail.com>
2023-12-08 19:31:10 +0530

    hero section data

--- sanity/schema.js
+++ sanity/schema.js
@@ -1,6 +1,7 @@
 import { blockContent } from "./schemas/blockContent";
 import { blogpost } from "./schemas/blogpost";
+import { heroslider } from "./schemas/heroslider";
 
 export const schema = {
-  types: [blogpost, blockContent],
+  types: [blogpost, blockContent, heroslider],
 };
